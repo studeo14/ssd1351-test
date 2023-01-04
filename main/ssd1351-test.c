52109829bab240fc8d331d0e288debc7b31ad6ec
Steven Frederiksen <studeo14@vt.edu>
2023-01-03 22:30:25 -0500

    Added basic cap touch sensing.
    Added basic cap touch sensing on pin A2 (GPIO9).

--- main/ssd1351-test.c
+++ main/ssd1351-test.c
@@ -6,6 +6,7 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 
+#include "touch_element/touch_button.h"
 
 #include <stdio.h>
 #define SPI_HOST SPI2_HOST
@@ -21,6 +22,35 @@
 
 static const char *TAG = "SSD1351 TEST";
 
+#define TOUCH_BUTTON 9
+
+static touch_button_handle_t button_handle;
+static const touch_pad_t button_channel = TOUCH_PAD_NUM9;
+static const float channel_sense = 0.1F;
+
+/* Button event handler task */
+static void button_handler_task(void *arg)
+{
+    (void) arg; //Unused
+    touch_elem_message_t element_message;
+    while (1) {
+        /* Waiting for touch element messages */
+        touch_element_message_receive(&element_message, portMAX_DELAY);
+        if (element_message.element_type != TOUCH_ELEM_TYPE_BUTTON) {
+            continue;
+        }
+        /* Decode message */
+        const touch_button_message_t *button_message = touch_button_get_message(&element_message);
+        if (button_message->event == TOUCH_BUTTON_EVT_ON_PRESS) {
+            ESP_LOGI(TAG, "Button[%ld] Press", (uint32_t)element_message.arg);
+        } else if (button_message->event == TOUCH_BUTTON_EVT_ON_RELEASE) {
+            ESP_LOGI(TAG, "Button[%ld] Release", (uint32_t)element_message.arg);
+        } else if (button_message->event == TOUCH_BUTTON_EVT_ON_LONGPRESS) {
+            ESP_LOGI(TAG, "Button[%ld] LongPress", (uint32_t)element_message.arg);
+        }
+    }
+}
+
 void app_main(void)
 {
     uint8_t text_x = 0, text_y = 0;
@@ -33,6 +63,31 @@ void app_main(void)
     ssd1306_clearScreen8();
     ssd1306_setColor(RGB_COLOR8(255, 255, 0));
 
+    touch_elem_global_config_t global_config = TOUCH_ELEM_GLOBAL_DEFAULT_CONFIG();
+    ESP_ERROR_CHECK(touch_element_install(&global_config));
+    ESP_LOGI(TAG, "Touch element lib installed");
+    touch_button_global_config_t button_global_config = TOUCH_BUTTON_GLOBAL_DEFAULT_CONFIG();
+    ESP_ERROR_CHECK(touch_button_install(&button_global_config));
+    ESP_LOGI(TAG, "Touch button installed");
+
+    touch_button_config_t button_config = {
+        .channel_num = button_channel,
+        .channel_sens = channel_sense
+    };
+
+    /* Create Touch buttons */
+    ESP_ERROR_CHECK(touch_button_create(&button_config, &button_handle));
+    /* Subscribe touch button events (On Press, On Release, On LongPress) */
+    ESP_ERROR_CHECK(touch_button_subscribe_event(button_handle, TOUCH_ELEM_EVENT_ON_PRESS | TOUCH_ELEM_EVENT_ON_RELEASE | TOUCH_ELEM_EVENT_ON_LONGPRESS,
+                                                    (void *)button_channel));
+    /* Set EVENT as the dispatch method */
+    ESP_ERROR_CHECK(touch_button_set_dispatch_method(button_handle, TOUCH_ELEM_DISP_EVENT));
+    /* Set LongPress event trigger threshold time */
+    ESP_ERROR_CHECK(touch_button_set_longpress(button_handle, 2000));
+    /* Create a handler task to handle event messages */
+    xTaskCreate(&button_handler_task, "button_handler_task", 4 * 1024, NULL, 5, NULL);
+    touch_element_start();
+    ESP_LOGI(TAG, "Touch buttons created");
     while(1) {
         ssd1306_printFixed8(text_x, text_y, "Hello World!", STYLE_NORMAL);
         vTaskDelay(10 / portTICK_PERIOD_MS);
